cbc6d65055dbfae4a5d85ed6b0b341c55bf097fa
Justin Jakob <justin.jakob391@outlook.com>
2024-12-19 13:18:48 +0100

    Added script syntax concept

--- Manila.CLI/ManilaCLI.cs
+++ Manila.CLI/ManilaCLI.cs
@@ -6,10 +6,19 @@ if (!Directory.Exists("./run/")) Directory.CreateDirectory("./run/");
 Directory.SetCurrentDirectory("./run/");
 #endif
 
-var app = new CommandApp();
-app.Configure(c => {
-	c.SetApplicationName("manila");
-	c.SetApplicationVersion(Manila.Manila.VERSION);
-});
+if (args.Length == 0 || !args[0].StartsWith(':')) {
+	var app = new CommandApp();
+	app.Configure(c => {
+		c.SetApplicationName("manila");
+		c.SetApplicationVersion(Manila.Manila.VERSION);
+	});
 
-app.Run(args);
+	app.Run(args);
+
+	return;
+}
+
+// Treat execution as a task execution
+var task = args[0][1..];
+
+Console.WriteLine("Executing task: " + task);
---
+++ README.md
@@ -0,0 +1,15 @@
+# Manila
+Manila is a build system for C++ using JavaScript for Build Scripts (maybe TypeScript support in the future).
+
+## Project Structure
+- **Manila/**
+The main sources of the build system
+
+- **Manila.API/**
+Contains the binding between the Build System and its scripting API
+
+- **Manila.CLI/**
+The executeable that will get shipped and runs the build system from command line
+
+- **run/**
+A test directory that is used in Debug to test the features of the build system
