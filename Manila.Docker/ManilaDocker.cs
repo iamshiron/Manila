56b4f0b3b158695c9059b13818f325e1f48a7825
Justin Jakob <justin.jakob391@outlook.com>
2025-01-29 12:47:33 +0100

    Added plugin main class

---
+++ Manila.Docker/ManilaDocker.cs
@@ -0,0 +1,20 @@
+﻿using Shiron.Manila.Ext;
+
+namespace Manila.Docker;
+
+public class ManilaDocker : ManilaPlugin {
+	[PluginInstance]
+	public static ManilaDocker instance { get; set; }
+
+	public ManilaDocker() : base("shiron.manila", "docker", "0.0.0") {
+	}
+
+	public override void init() {
+		instance.release();
+		debug("ManilaDocker.init()");
+	}
+
+	public override void release() {
+		debug("ManilaDocker.release()");
+	}
+}
