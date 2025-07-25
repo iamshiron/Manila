9ffd1c79f397548987de5e035e4fd23d8006a82b
Justin Jakob <justin.jakob391@outlook.com>
2025-07-25 21:52:01 +0200

    Created Manila.JS plugin project

---
+++ Manila.JS/ManilaJS.cs
@@ -0,0 +1,13 @@
+﻿
+using Shiron.Manila.Ext;
+
+namespace Shiron.Manila.JS;
+
+public class ManilaJS() : ManilaPlugin("shiron.manila", "js", "1.0.0", ["Shiron"], []) {
+    public override void Init() {
+        Debug("Init");
+    }
+    public override void Release() {
+        Debug("Release");
+    }
+}
